a33c23eaa5ccb0aed08a6bc86762d45ab94c5b09
emepar <dimovt71@gmail.com>
2025-05-23 16:20:45 +0300

    Create main.py
    +window and window settings

---
+++ main.py
@@ -0,0 +1,10 @@
+from pygame import *
+
+#window
+window_heigh = 960
+window_width = 1280
+
+window = display.set_mode((window_width, window_heigh))
+display.set_caption("Ping Pong")
+background = (255, 255, 255)
+window.fill(background)
