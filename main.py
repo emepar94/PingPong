23d0f038748a616392736cb17ea23d0f7101ae05
emepar <dimovt71@gmail.com>
2025-05-23 16:33:34 +0300

    Update main.py
    +game cycle

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ window = display.set_mode((window_width, window_heigh))
 display.set_caption("Ping Pong")
 background = (255, 255, 255)
 window.fill(background)
+
+clock = time.Clock()
+FPS = 190
+
+game = True
+while game:
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                game = False
+
+    display.update()
+    clock.tick(FPS)
