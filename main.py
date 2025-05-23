8368ba19d576853d83eaeb2649cdd096612a1974
emepar <dimovt71@gmail.com>
2025-05-23 16:50:03 +0300

    Update main.py
    +superclass

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, pos_x, pos_y, size_x, size_y, speed, picture):
+        super().__init__()
+        self.image = transform.scale(image.load(picture), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.speed = speed
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 #window
 window_heigh = 960
 window_width = 1280
